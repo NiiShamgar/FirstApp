4fc60bcad7b4e1057add25a9392daba7b4a22fed
unknown <niiaddoac@gmail.comgit config --global user.name NiiShamgar>
2020-11-18 16:46:18 +0000

    delete something.js

---
+++ something.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {Text, View, Image, StyleSheet} from 'react-native';
+
+function App(){
+  return <View style={styles.container}>
+     <Image source={require('./assets/profile.jpg')} style={styles.image}/>  
+  
+  <View style={styles.detailContainer}>
+    <Text style={styles.label}>Name</Text>
+    <Text style={styles.label,styles.info}>Tony Start</Text>
+  </View>
+ 
+ <View style={styles.detailContainer}>
+    <Text style={styles.label}>Email</Text>
+    <Text style={styles.label,styles.info}>everything@gmail.com</Text>
+  </View>
+
+<View style={styles.detailContainer}>
+  <Text style={styles.label}>Gender</Text>
+  <Text style={styles.label,styles.info}>Male</Text>
+</View>
+
+</View>
+
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    marginHorizontal:5
+  },
+
+  image:{
+    marginTop:100,
+    width:150,
+    height:150,
+    marginBottom:30,
+    alignSelf:"center",
+    borderRadius:75
+  },
+
+  detailContainer:{
+    flexDirection:"row",
+    marginVertical:3
+  },
+
+  label:{
+    borderColor:"black",
+    borderWidth:1,
+    fontSize:20,
+    flex:2.5,
+    paddingHorizontal:5
+  },
+
+  info:{
+    flex:7.5,
+  }
+
+});
+
+
+export default App;
